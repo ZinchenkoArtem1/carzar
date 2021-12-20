1d5afc77d08cd25841ace43a9591e12fee01d8d6
ArtemZinchenko <Artem.Zinchenko@livevision.com.ua>
2021-12-21 00:08:21 +0200

    fix user login

--- src/main/java/ua/com/zinchenko/carzar/api/UserController.java
+++ src/main/java/ua/com/zinchenko/carzar/api/UserController.java
@@ -2,9 +2,7 @@ package ua.com.zinchenko.carzar.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ua.com.zinchenko.carzar.model.User;
 import ua.com.zinchenko.carzar.service.UserService;
 
@@ -18,7 +16,8 @@ public class UserController {
         this.userService = userService;
     }
 
-    public ResponseEntity<?> userLogin(User user) {
+    @PostMapping("/login")
+    public ResponseEntity<?> userLogin(@RequestBody User user) {
         if(!userService.isUserExist(user)) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
